bf9d3473e57a15eeebe1415677c18adfb2bcbcad
Sergej Pyzin <sergejpyzin@yandex.ru>
2023-06-07 10:40:48 +0300

    finished taskOne

--- src/main/java/taskOne.java
+++ src/main/java/taskOne.java
@@ -1,2 +1,31 @@
+import java.util.Scanner;
+
 public class taskOne {
+    //    1. Задать одномерный массив и найти в нем минимальный и максимальный элементы ;
+    public static int[] fillingArray() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите количество элементов массива: ");
+        int quantityElement = scanner.nextInt();
+        int[] someArray = new int[quantityElement];
+        for (int i = 0; i < someArray.length; i++) {
+            System.out.print("Введите елемент массива: ");
+            someArray[i] = scanner.nextInt();
+        }
+        return someArray;
+    }
+
+    public static void main(String[] args) {
+        int[] array = fillingArray();
+        int min = array[0];
+        int max = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            } else if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        System.out.println("min = " + min);
+        System.out.println("max = " + max);
+    }
 }
