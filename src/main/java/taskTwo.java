a2b54653afd5687b6624853579d196275e4a06ce
Sergej Pyzin <sergejpyzin@yandex.ru>
2023-06-07 10:49:39 +0300

    changed taskTwo

--- src/main/java/taskTwo.java
+++ src/main/java/taskTwo.java
@@ -3,27 +3,27 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class taskTwo {
-    private static int[][] fillingTwoDimensionalArray(){
+    private static int[][] fillingMatrix(){
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
         System.out.print("Введите количество строк: ");
         int row = scanner.nextInt();
         System.out.print("Введите количество столбцов: ");
         int columns = scanner.nextInt();
-        int[][] someArray = new int[row][columns];
+        int[][] someMatrix = new int[row][columns];
 
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < columns; j++) {
-                if (j == i) someArray[i][j] = 1;
-                else if (j == columns - i - 1) someArray[i][j] = 1;
-                else someArray[i][j] = random.nextInt(3, 10);
+                if (j == i) someMatrix[i][j] = 1;
+                else if (j == columns - i - 1) someMatrix[i][j] = 1;
+                else someMatrix[i][j] = random.nextInt(3, 10);
             }
         }
-        return someArray;
+        return someMatrix;
 
     }
     public static void main(String[] args) {
-        int[][] array = fillingTwoDimensionalArray();
-        System.out.println(Arrays.deepToString(array));
+        int[][] matrix = fillingMatrix();
+        System.out.println(Arrays.deepToString(matrix));
     }
 }
