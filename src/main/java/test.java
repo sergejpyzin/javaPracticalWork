120ecaa1396bb1953e594763a3c0459e2947b49f
Sergej Pyzin <sergejpyzin@yandex.ru>
2023-06-07 11:12:07 +0300

    created test

---
+++ src/main/java/test.java
@@ -0,0 +1,6 @@
+public class test {
+    public static void main(String[] args) {
+        System.out.println(java.time.ZonedDateTime.now());
+        System.out.println(java.time.LocalDateTime.now());
+    }
+}
