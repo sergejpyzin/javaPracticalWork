6a63de89d7a7e58a689ffbc910033da130ae8c90
Sergej Pyzin <sergejpyzin@yandex.ru>
2023-06-07 12:39:06 +0300

    finished taskThree

--- src/main/java/taskThree.java
+++ src/main/java/taskThree.java
@@ -1,2 +1,33 @@
+import java.time.LocalTime;
+import java.util.Scanner;
+
 public class taskThree {
-}
+//    В консоли запросить имя пользователя. В зависимости от текущего времени, вывести приветствие вида:
+//            "Доброе утро, <Имя>!", если время от 05:00 до 11:59
+//            "Добрый день, <Имя>!", если время от 12:00 до 17:59;
+//"Добрый вечер, <Имя>!", если время от 18:00 до 22:59;
+//"Доброй ночи, <Имя>!", если время от 23:00 до 4:59
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        LocalTime morningTime = LocalTime.of(11, 59);
+        LocalTime dayTime = LocalTime.of(17, 59);
+        LocalTime eveningTime = LocalTime.of(22, 59);
+        LocalTime nightTime = LocalTime.of(4, 59);
+        LocalTime localTime = LocalTime.now();
+
+        System.out.println("Введите имя пользователя:");
+        String userName = scanner.next();
+
+        if ((localTime.isAfter(nightTime) && localTime.isBefore(morningTime)) || localTime.equals(morningTime))
+            System.out.println("Доброе утро, " + userName);
+        else if ((localTime.isAfter(morningTime) && localTime.isBefore(dayTime)) || localTime.equals(dayTime))
+            System.out.println("Добрый день, " + userName);
+        else if ((localTime.isAfter(dayTime) && localTime.isBefore(eveningTime)) || localTime.equals(eveningTime))
+            System.out.println("Добрый вечер, " + userName);
+        else System.out.println("Доброй ночи, " + userName);
+
+
+
+        }
+
+    }
--- src/main/java/test.java
+++ src/main/java/test.java
@@ -1,6 +1,13 @@
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
+
 public class test {
     public static void main(String[] args) {
-        System.out.println(java.time.ZonedDateTime.now());
-        System.out.println(java.time.LocalDateTime.now());
+//        System.out.println(java.time.ZonedDateTime.now());
+//        System.out.println(java.time.LocalDateTime.now());
+        LocalDateTime date = LocalDateTime.now();
+        ZonedDateTime zoneDate = ZonedDateTime.now();
+        System.out.println(zoneDate);
+
     }
 }
