4e00c568b55b377377eb1319e5b7abbc7fc5a8db
Sergej Pyzin <sergejpyzin@yandex.ru>
2023-06-24 11:33:31 +0300

    create PracticalTaskLessonFifth

---
+++ src/main/java/PracticalTaskLessonFifth/TelefoneDirectory.java
@@ -0,0 +1,4 @@
+package PracticalTaskLessonFifth;
+
+public class TelefoneDirectory {
+}
--- src/main/java/test.java
+++ src/main/java/test.java
@@ -1,55 +1,76 @@
-import java.util.Collection;
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.Scanner;
+import java.util.*;
 
 public class test {
-    /*
-    Реализовать консольное приложение, которое:
-1. Принимает от пользователя и “запоминает” строки.
-2. Если введено print, выводит строки так, чтобы последняя введенная была первой в списке, а первая - последней.
-3. Если введено revert, удаляет предыдущую введенную строку из памяти.
-4. Если введено exit, то программа завершается
-
-> - ввод в консоль (stdin), < - вывод на консоль (stdout)
-> java
-> python
-> c#
-> print
-< [c#, python, java]
-> revert
-> print
-< [python, java]
-> revert
-> revert
-> print
-< []
-> revert -> throw new NoSuchElementException
-     */
-
     public static void main(String[] args) {
-        LinkedList<String> linkedList = new LinkedList<>();
         Scanner scanner = new Scanner(System.in);
-        String massage;
+        HashMap<String, ArrayList<String>> telephoneDirectory = new HashMap<>();
+        ArrayList<String> telephoneNumbers;
+        String inputCommand;
         System.out.println("""
-                Команды для взаимодействия:
-                1. print - показать ранее введенные строки;
-                2. revert - удалить последнюю введенную строку;
-                3. exit - выйти из программы.""");
+                Здравствуйте пользователь вы находитесь в телефонном справочнике.
+                Для взаимодействием со справочником имеются команды:
+                1. ADD Lastname Phone-number - добавление телефонного номера в справочник. Phone-number - вводится без пробелов.
+                    Пример: ADD Ivanov 99999999999;
+                2. GET Lastname - получить все телефонные номера относящиеся к данному абоненту.
+                    Пример: GET Ivanov;
+                3. REMOVE Lastname - удалить абонента из телефонной книги;
+                    Пример: REMOVE Ivanov;
+                4. LIST - вывести всю информацию;
+                5. EXIT - выход из программы.
+                """);
 
-        do {
-            System.out.println("Введите строку:");
-            massage = scanner.next();
-            if (massage.equalsIgnoreCase("print")) {
-                System.out.println(linkedList);
-            } else if (massage.equalsIgnoreCase("revert")) {
-                linkedList.removeFirst();
-            } else {
-                linkedList.addFirst(massage);
+        while (true) {
+            System.out.println("Введите команду:");
+            inputCommand = scanner.nextLine();
+            String[] enteredMessage = inputCommand.trim().split(" ");
+            String enteredCommands = enteredMessage[0];
+            String enteredUser;
+            String enteredTelephone;
+            if (enteredCommands.equalsIgnoreCase("exit")) {
+                System.out.println("Завершение работы программы.");
+                break;
             }
-        } while (!massage.equalsIgnoreCase("exit"));
-
-
+            if (enteredCommands.equalsIgnoreCase("add")) {
+                enteredUser = enteredMessage[1];
+                enteredTelephone= enteredMessage[2];
+                if (enteredMessage.length != 3) {
+                    System.out.println("Ошибка ввода. Попробуйте еще раз.");
+                    continue;
+                }
+                if (telephoneDirectory.get(enteredUser) == null) {
+                    telephoneNumbers = new ArrayList<>();
+                } else {
+                    telephoneNumbers = telephoneDirectory.get(enteredUser);
+                }
+                telephoneNumbers.add(enteredTelephone);
+                telephoneDirectory.put(enteredUser, telephoneNumbers);
+            }
+            if (enteredCommands.equalsIgnoreCase("get")) {
+                enteredUser = enteredMessage[1];
+                if (telephoneDirectory.get(enteredUser) == null) {
+                    System.out.println("Не найдена запись с фамилией " + enteredUser);
+                } else {
+                    telephoneNumbers = telephoneDirectory.get(enteredUser);
+                    System.out.println(enteredUser + " -> " + telephoneNumbers);
+                }
+            }
+            if (enteredCommands.equalsIgnoreCase("remove")) {
+                enteredUser = enteredMessage[1];
+                if (telephoneDirectory.get(enteredUser) == null) {
+                    System.out.println("Не найдена запись с фамилией " + enteredUser);
+                } else {
+                    telephoneDirectory.remove(enteredUser);
+                    System.out.println("Запись с фамилией " + enteredUser + " удалена");
+                }
+            }
+            if (enteredCommands.equalsIgnoreCase("list")) {
+                if (telephoneDirectory.isEmpty()) {
+                    System.out.println("Не найдено ни одной записи в телефонном справочнике");
+                } else {
+                    System.out.println(telephoneDirectory);
+                }
+            }
+        }
     }
 }
 
