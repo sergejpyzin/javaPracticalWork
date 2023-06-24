0aee4e37cd363bf51fb3766b1f84d4606e929e28
Sergej Pyzin <sergejpyzin@yandex.ru>
2023-06-24 18:58:14 +0300

    changed Main class

--- src/main/java/PracticalTaskLessonSix/Main.java
+++ src/main/java/PracticalTaskLessonSix/Main.java
@@ -1,4 +1,17 @@
 package PracticalTaskLessonSix;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
+    public static void main(String[] args) {
+        List<Notebook> notebooks = new ArrayList<>();
+
+        notebooks.add(new Notebook("Intel", "Red", 16));
+        notebooks.add(new Notebook("Athlon", "Grey", 8));
+        notebooks.add(new Notebook("Dell", "Steel", 32));
+
+        System.out.println(notebooks);
+    }
 }
